0dcd4eeb5d2a0c01a492b2a5a100d4c6992e8959
billyct <billy_allen@126.com>
2013-08-19 09:33:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ exports.config = {
 	version: '0.0.1',
 	host: '127.0.0.1',
 	port: 3000,
-	db: 'mongodb://billyct:4179965@127.0.0.1/soonseen'
+	db: 'mongodb://username:secret@127.0.0.1/soonseen'
 };
